8a4678daac46e987a8023651c313921806c1a2a3
Thang Tran <37521149+thangtran3112@users.noreply.github.com>
2024-12-04 17:47:45 -0800

    make model path relative

--- annclassification/app.py
+++ annclassification/app.py
@@ -6,16 +6,16 @@ import pandas as pd
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('./model.h5')
 
 # Load the encoders and scaler
-with open('label_encoder_gender.pkl', 'rb') as file:
+with open('./label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl', 'rb') as file:
+with open('./onehot_encoder_geo.pkl', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
+with open('./scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
